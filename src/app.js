e901a39ba41a6993b98bcf1b842782848c25f260
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2020-06-20 19:13:39 +0530

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -19,19 +19,10 @@ import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: "Water Bill", amount: 4900 }));
-store.dispatch(addExpense({ description: "Gas Bill", amount: 4500 }));
-store.dispatch(
-  addExpense({ description: "Rent", amount: 900, createdAt: 1000 })
-);
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 );
+
 ReactDOM.render(jsx, document.getElementById("app"));
--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import { ExpenseListFilters } from "../../components/ExpenseListFilters";
 import { filters, altFilters } from "../fixtures/filter";
+import moment from "moment";
 
 let setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate, wrapper;
 
@@ -42,10 +43,38 @@ test("should handle text change", () => {
   expect(setTextFilter).toHaveBeenLastCalledWith(value);
 });
 
-test("should sort by date", () => {});
+test("should sort by date", () => {
+  const value = "date";
+  wrapper.setProps({
+    filters: altFilters,
+  });
+  wrapper.find("select").simulate("change", {
+    target: { value },
+  });
+  expect(sortByDate).toHaveBeenCalled();
+});
 
-test("should sort by amount", () => {});
+test("should sort by amount", () => {
+  const value = "amount";
+  wrapper.setProps({
+    filters: altFilters,
+  });
+  wrapper.find("select").simulate("change", {
+    target: { value },
+  });
+  expect(sortByAmount).toHaveBeenCalled();
+});
 
-test("should handle date changes", () => {});
+test("should handle date changes", () => {
+  const startDate = moment(0).add(4, "years");
+  const endDate = moment(0).add(8, "years");
+  wrapper.find("DateRangePicker").prop("onDatesChange")({ startDate, endDate });
+  expect(setStartDate).toHaveBeenLastCalledWith(startDate);
+  expect(setEndDate).toHaveBeenLastCalledWith(endDate);
+});
 
-test("should handle date focus changes", () => {});
+test("should handle date focus changes", () => {
+  const calendarFocused = 'endDate';
+  wrapper.find('DateRangePicker').prop('onFocusChange')(calendarFocused);
+  expect(wrapper.state('calendarFocused')).toBe(calendarFocused);
+});
